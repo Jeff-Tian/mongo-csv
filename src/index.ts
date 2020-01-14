5bb32e59169feedb381b463bd1d17a4cf170e8df
Jeff Tian <jie.tian@unicareer.com>
2020-01-14 16:11:19 +0800

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 #! /usr/bin/env node
 
-import { Argv } from 'yargs';
 import { Db, MongoClient } from 'mongodb';
+import { Argv } from 'yargs';
 
-const config = require('./config.json');
+import config from './config.json';
+
 let client: MongoClient | null = null;
-let db: Db | null = null;
+const db: Db | null = null;
+
+const connect = async () => {
+  console.log('connecting...');
+  client = new MongoClient(config.mongo.uri, { useUnifiedTopology: true });
+  console.log('client = ', client);
+  await client.connect();
+  console.log('connected.');
+};
 
 // tslint:disable-next-line
 require('yargs')
@@ -17,18 +26,27 @@ require('yargs')
     (yargs: Argv) => yargs,
     (args: any) => {
       (async () => {
-        console.log('connecting...');
-        client = new MongoClient(config.mongo.uri, { useUnifiedTopology: true });
-        console.log('client = ', client);
-        await client.connect();
-        console.log('connected.');
-        await client.close();
+        await connect();
+        if (client && client.isConnected) {
+          await client.close();
+        }
       })().then(() => {
         console.log('done with connect command');
       });
     },
   )
-  .command(['ls'], 'list databases', (yargs: Argv) => yargs, (args: any) => {})
+  .command(
+    ['export'],
+    'exports',
+    (yargs: Argv) => yargs,
+    (args: any) => {
+      (async () => {
+        await connect();
+      })().then(() => {
+        console.log('done with export command');
+      });
+    },
+  )
   .command(
     ['config'],
     'output the current config',
